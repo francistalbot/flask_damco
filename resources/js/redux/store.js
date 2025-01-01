0158e8286b75cbb8d32274a9390dba17be6abb4d
francistalbot <francistalbot@hotmail.com>
2025-01-01 17:52:46 -0500

    Create redux slice for the search

---
+++ resources/js/redux/store.js
@@ -0,0 +1,9 @@
+// src/redux/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import searchSlice from "./searchSlice";
+
+export const store = configureStore({
+    reducer: {
+        search: searchSlice,
+    },
+});
